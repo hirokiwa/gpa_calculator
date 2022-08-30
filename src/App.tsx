17ddf697bedf880960a109e6bc8665bb3d312869
hirokiwa <hirokifsys@gmail.com>
2022-08-30 15:59:04 +0900

    kill zoom in

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './page/home/Home';
 function App() {
   return (
     <div className='App'>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"></meta>
       <BrowserRouter>
         <Routes>
           <Route path={`/`} element={<Home />} />
