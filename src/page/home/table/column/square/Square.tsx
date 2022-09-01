6247d7e8ae60fedc58a1a067d628d5e1c740e11e
hirokiwa <hirokifsys@gmail.com>
2022-09-01 15:48:15 +0900

    set select style

--- src/page/home/table/column/square/Square.tsx
+++ src/page/home/table/column/square/Square.tsx
@@ -8,24 +8,28 @@ function Square(props:Square_type) {
   return (
     <td className='class_box'>
         <div>
-            <select id={"grade"+props.id_number}>
-                <option value="" selected disabled>評価　</option>
-                <option value="5">A+</option>
-                <option value="4">A</option>
-                <option value="3">B</option>
-                <option value="2">C</option>
-                <option value="0">F</option>
-            </select>
+            <div className='select_outer'>
+                <select id={"grade"+props.id_number}>
+                    <option value="" selected disabled>評価　</option>
+                    <option value="5">A+</option>
+                    <option value="4">A</option>
+                    <option value="3">B</option>
+                    <option value="2">C</option>
+                    <option value="0">F</option>
+                </select>
+            </div>
         </div>
         <div>
-            <select id={"credit"+props.id_number}>
-                <option value="" selected disabled>単位数</option>
-                <option>0</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
+            <div className='select_outer'>
+                <select id={"credit"+props.id_number}>
+                    <option value="" selected disabled>単位数</option>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+            </div>
         </div>
     </td>
   );
