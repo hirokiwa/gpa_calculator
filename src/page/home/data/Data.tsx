cdffb9521f9ea3df38bcc8d5b86cdc7de814c668
hirokiwa <hirokifsys@gmail.com>
2022-09-01 21:33:32 +0900

    allow 0.00

--- src/page/home/data/Data.tsx
+++ src/page/home/data/Data.tsx
@@ -17,13 +17,13 @@ function Data(props:type_data) {
     const Gpa = () =>{
         let data = Array(30);
         
-        for(let i = 0; i < 30; i++){
+        for(let i:number = 0; i < 30; i++){
             data[i] = Array(2).fill(0);
             data[i][0] = document.getElementById("grade"+String(i));
             data[i][1] = document.getElementById("credit"+String(i));
         }
     
-        for(let i = 0; i < 30; i++){
+        for(let i:number = 0; i < 30; i++){
             console.log(data[i][0].value);
             console.log(data[i][1].value);
             if(data[i][0].value !== data[i][1].value){
@@ -35,21 +35,20 @@ function Data(props:type_data) {
             }
         }
     
-        let sum_score = 0;
-        let sum_credit = 0;
+        let sum_score:number = 0;
+        let sum_credit:number = 0;
         // calculate
         for(let i = 0; i < 30; i++){
             sum_score = sum_score + data[i][0].value * data[i][1].value;
             sum_credit = sum_credit + Number(data[i][1].value);
         }
 
-        if(sum_credit === 0){
-            alert('正しく入力してください。')
-            return
+        let GPA:number = 0;
+
+        if(sum_credit !== 0){
+            GPA = sum_score / sum_credit;
+            GPA = Math.floor(GPA * Math.pow(10, 2)) / Math.pow(10, 2);
         }
-    
-        let GPA = sum_score / sum_credit;
-        GPA = Math.floor(GPA * Math.pow(10, 2)) / Math.pow(10, 2);
         
         console.log(sum_score);
         console.log(sum_credit);
